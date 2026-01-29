16ccb0b9332e5e5c821bd31d7c8a73d0f0d73b01
ivankorolev <ikorolev368@gmail.com>
2026-01-29 14:44:09 +0300

    add db init and config

---
+++ src/config/config/config.py
@@ -0,0 +1,29 @@
+import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+
+    app_name:str = "gRPC_fastapi_app"
+
+    app_prefix:str = "/grpc_dastapi"
+
+    mongo_user:str = os.getenv("MONGODB_USER_LOGIIN")
+    mongo_password:str = os.getenv("MONGODB_USER_PASSWPRD")
+    mongo_base_name: str = os.getenv("MONGODB_DATABASE_NAME")
+
+    mongo_url:str = " "
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.mongodb_url = (
+        f"mongodb://{self.mongo_user}:{self.mongo_password}"
+        f"@mongodb:27017/{self.mongo_base_name}"
+        f"?authSource={self.mongo_base_name}" )
+
+
+settings = Settings()
