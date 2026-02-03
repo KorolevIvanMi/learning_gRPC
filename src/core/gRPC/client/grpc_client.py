07b8161f5ecfbc8738273ea0fefd18bca9f04cfa
ivankorolev <Ivankorol1512@yandex.ru>
2026-02-03 14:12:09 +0300

    add grpc client

---
+++ src/core/gRPC/client/grpc_client.py
@@ -0,0 +1,26 @@
+import grpc
+from proto import catalog_pb2_grpc
+
+
+class GRPCClient:
+    def __init__(self):
+        self.channel = None
+        self.stub = None
+
+
+    async def connect(self):
+        self.channel = grpc.aio.insecure_channel(f"localhost:{50051}")
+        self.stub = catalog_pb2_grpc.CatologStub(self.channel)
+        return self
+
+
+    async def disconnect(self):
+        if self.channel:
+            await self.channel.close()
+
+
+    def get_stub(self):
+        return self.stub
+
+
+grpc_client = GRPCClient()
